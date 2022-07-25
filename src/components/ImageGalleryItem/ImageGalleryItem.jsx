02ca632bf56f7b9c6dd88e36a6359576193676fb
Dmitry Davidyk <91990771+DmitryDavidyuk@users.noreply.github.com>
2022-07-26 00:33:13 +0300

    ПРОЩЕ УДАЛИТЬ И НАПИСАТЬ ВСЕ С НУЛЯ

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
-import CSS from './ImageGalleryItem.module.css';
+import PropTypes from 'prop-types';
+import styles from './ImageGalleryItem.module.scss';
 
-const ImageGalleryItem = ({ images }) => {
-  return images.map(image => (
-    <li key={image.id} className={CSS.ImageGalleryItem}>
+const ImageGalleryItem = ({ image, onImageClick }) => {
+  const fullImage = () => onImageClick(image.largeImageURL);
+
+  return (
+    <li className={styles.ImageGalleryItem}>
       <img
-        className={CSS.ImageGalleryItem_image}
         src={image.webformatURL}
-        alt=""
+        alt={image.tags}
+        className={styles['ImageGalleryItem-image']}
+        onClick={fullImage}
       />
     </li>
-  ));
+  );
+};
+
+ImageGalleryItem.defaultProps = {
+  tags: '',
+};
+
+ImageGalleryItem.propTypes = {
+  image: PropTypes.shape({
+    webformatURL: PropTypes.string.isRequired,
+    largeImageURL: PropTypes.string.isRequired,
+    tags: PropTypes.string,
+  }),
+  onImageClick: PropTypes.func.isRequired,
 };
 
 export default ImageGalleryItem;
